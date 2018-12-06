fffdb51bfc5b17849d432a8b6f1105756514463c
Müller <rmueller1@MUERO-AMBP.local>
2018-12-06 14:59:16 +0100

    initial push

---
+++ src/main/java/com/senacor/deploymenttest/DeploymentTestApplication.java
@@ -0,0 +1,12 @@
+package com.senacor.deploymenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentTestApplication.class, args);
+    }
+}
